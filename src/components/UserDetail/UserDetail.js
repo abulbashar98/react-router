ff5fa914e185d62e87e4845fbb8ff4c7f85babd0
abulbashar98 <abulbashar98116@gmail.com>
2022-03-30 18:04:34 +0600

    Readme.md updated for dynamic part and useParams() hook

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserDetail = () => {
-    const params = useParams()
+    // const params = useParams()
+    const { userId } = useParams()
     return (
         <div>
-            <h1>Detail of User: {params.userId}</h1>
+            {/* <h1>Detail of User: {sparams.userId}</h1> */}
+            <h1>Detail of User: {userId}</h1>
         </div>
     );
 };
