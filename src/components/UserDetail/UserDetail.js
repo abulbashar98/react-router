65776696779df103d7952a31607fa9078a4f1f2e
abulbashar98 <abulbashar98116@gmail.com>
2022-03-31 00:27:06 +0600

    Load and Use Data using Dynamic url parameter, USe to set dynamic part of url... Then use the loaded to show dynamic information in one Component for the path we set onClick and also in <Link> Component

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserDetail = () => {
     // const params = useParams()
     const { userId } = useParams()
+
+    const [user, setUser] = useState({})
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${userId}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, [userId])
+
     return (
         <div>
             {/* <h1>Detail of User: {sparams.userId}</h1> */}
             <h1>Detail of User: {userId}</h1>
+            <h2>Name: {user.name}</h2>
+            <h2>Email {user.email}</h2>
+            <h4> website{user.website}</h4>
+            <h5>Phone: {user.phone}</h5>
+            <h5>address: {user?.address?.city}</h5>
+            <p><small>Latitude: {user?.company?.catchPhrase}</small></p>
         </div>
     );
 };
